6f6d4b2837f6ee0c772620dd57fde0fe50488fa5
Nimisha Yadav <73909578+nimisha-yadav@users.noreply.github.com>
2021-03-03 04:41:34 +0530

    Create Ques2.java

---
+++ Shreeraksha/Nimisha/Array/Ques2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + " ");
+    }
+    System.out.println(sb);
+  }
+
+  public static void reverse(int[] a){
+    int li = 0;
+    int ri = a.length - 1;
+
+    while(li < ri){
+      int temp = a[li];
+      a[li]= a[ri];
+      a[ri] = temp;
+      
+      li++;
+      ri--;
+    }
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    reverse(a);
+    display(a);
+ }
+}
